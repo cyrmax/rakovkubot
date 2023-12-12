6226dbac206c2e1d419d9379596c9060c8a1a572
Kirill Belousov <cyrmax@internet.ru>
2023-12-12 12:19:10 +0300

    Implemented basic logging to console. Log level set to Debug, cannot be changed via variables.

--- bot.cs
+++ bot.cs
@@ -1,29 +1,48 @@
-﻿using System.Security;
-using System.Text;
+﻿using System.Text;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Microsoft.Extensions.Logging;
 
 namespace RakovkuBot;
 
 class Program
 {
+    private static ILoggerFactory? loggerFactory;
+    private static ILogger? logger;
     public static async Task Main(string[] args)
     {
+        loggerFactory = LoggerFactory.Create((ILoggingBuilder builder) =>
+        {
+            builder
+            .SetMinimumLevel(LogLevel.Trace)
+            .AddConsole();
+        });
+        logger = loggerFactory?.CreateLogger("RakovkuBot");
+        logger?.LogInformation("Initializing Rakovku bot");
+        AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler((object sender, UnhandledExceptionEventArgs args) =>
+        {
+            logger?.LogCritical((args.ExceptionObject as Exception), "An unhandled exception occured");
+        });
         string? botToken = Environment.GetEnvironmentVariable("RAKOVKU_BOT_TOKEN");
-        if (botToken is null) return;
+        if (botToken is null)
+        {
+            logger?.LogCritical("Unable to get RAKOVKU_BOT_TOKEN environment variable");
+            return;
+        }
         var bot = new TelegramBotClient(botToken);
         using CancellationTokenSource cts = new CancellationTokenSource();
         ReceiverOptions receiverOptions = new() { AllowedUpdates = { } };
         bot.StartReceiving(updateHandler: botOnUpdate, pollingErrorHandler: botOnError, receiverOptions: receiverOptions, cancellationToken: cts.Token);
         var me = await bot.GetMeAsync();
-        Console.WriteLine($"Started Rakovku bot. User ID is {me.Id}, username is {me.Username}");
+        logger?.LogInformation("Rakovku Bot started. Bot username: {}, bot user ID: {}", me.Username, me.Id);
         bool shouldTerminate = false;
         Console.CancelKeyPress += new ConsoleCancelEventHandler((sender, args) =>
         {
-            Console.WriteLine("Ctrl+C received. Terminating.");
+            logger?.LogInformation("Ctrl+C received. Terminating.");
             cts.Cancel();
+            loggerFactory?.Dispose();
             shouldTerminate = true;
         });
         while (!shouldTerminate) { }
@@ -31,15 +50,20 @@ class Program
 
     private static async Task botOnUpdate(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
     {
+        logger?.LogDebug("Received update of type {UpdateType}", update.Type);
         if (update.Type != UpdateType.Message) return;
         if (update.Message?.Text is not { } messageText) return;
+        logger?.LogDebug("Converting text message");
         string result = await Converter.ConvertAsync(messageText, cancellationToken);
+        logger?.LogDebug("Sending converted text message");
         await bot.SendTextMessageAsync(update.Message.Chat, result, replyToMessageId: update.Message.MessageId);
     }
 
-    private static async Task botOnError(ITelegramBotClient bot, Exception exception, CancellationToken cancellationToken)
+    // Here we do not use `async` keyword because the function does not do any awaitable operations. But we still need to return Task because `Telegram.Bot` architecture needs it. So we return `Task.CompletedTask` as a sort of stub.
+    private static Task botOnError(ITelegramBotClient bot, Exception exception, CancellationToken cancellationToken)
     {
-        Console.WriteLine($"An error has occured.\n{exception.Message}");
+        logger?.LogError(exception, "An error occured.");
+        return Task.CompletedTask;
     }
 }
 
